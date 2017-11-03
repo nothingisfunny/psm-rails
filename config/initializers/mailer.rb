8a2078297a328d6d70139f7bb6a2bb5755d3b205
nothingisfunny <if.the.kids.r.united@gmail.com>
2017-11-03 13:09:28 -0700

    change mailer initializer

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,10 +1,11 @@
 
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
+ActionMailer::Base.smtp_settings = {
   address:              'smtp.beget.com',
   port:                 587,
   domain:               'psm-spb.com',
   user_name:            'info',
   password:             'ydkSHrsH',
   authentication:       'plain',
-  enable_starttls_auto: true  }
+  enable_starttls_auto: true 
+}
+ActionMailer::Base.delivery_method = :smtp
