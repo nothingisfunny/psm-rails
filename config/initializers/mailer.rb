6c953efebb05a384c9ee2401338f30a417026b20
nothingisfunny <if.the.kids.r.united@gmail.com>
2017-11-03 13:06:25 -0700

    add active mailer configuration

---
+++ config/initializers/mailer.rb
@@ -0,0 +1,10 @@
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address:              'smtp.beget.com',
+  port:                 587,
+  domain:               'psm-spb.com',
+  user_name:            'info',
+  password:             'ydkSHrsH',
+  authentication:       'plain',
+  enable_starttls_auto: true  }
