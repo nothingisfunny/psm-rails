14bf44d1d1d37d9d7414a4ded5c7c0066b62c5a8
nothingisfunny <if.the.kids.r.united@gmail.com>
2017-11-07 16:16:19 -0800

    change db to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171103200936) do
+ActiveRecord::Schema.define(version: 20171107235535) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "messages", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -19,6 +22,11 @@ ActiveRecord::Schema.define(version: 20171103200936) do
     t.string "email"
     t.string "phone"
     t.string "content"
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
