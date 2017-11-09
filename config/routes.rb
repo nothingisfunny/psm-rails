b43735aad94be6fece150666114be8691ce6a9de
nothingisfunny <if.the.kids.r.united@gmail.com>
2017-11-08 22:44:59 -0800

    generate model, controller, and route for projects

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :partners, only: [:index]
   resources :reviews, only: [:index]
   resources :customers, only: [:index]
+  resources :projects, only: [:index]
   get 'contact', to: 'messages#new', as: 'contact'
   post 'contact', to: 'messages#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
