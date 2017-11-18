39a43240cc1188ba25a292b7814625711b7ca525
nothingisfunny <if.the.kids.r.united@gmail.com>
2017-11-18 11:39:52 -0800

    add review pictures

--- db/migrate/20171109062727_create_reviews.rb
+++ db/migrate/20171109062727_create_reviews.rb
@@ -1,7 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[5.1]
   def change
     create_table :reviews do |t|
-
+      t.string :name
+      t.string :description
+      t.string :document
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20171109064351) do
   end
 
   create_table "reviews", force: :cascade do |t|
+    t.string "name"
+    t.string "picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
