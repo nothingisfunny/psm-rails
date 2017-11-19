c3addd9c93c0f5ecf069496dfa865fcedaeda2fc
nothingisfunny <if.the.kids.r.united@gmail.com>
2017-11-18 21:24:48 -0800

    compile assets

--- app/mailers/automated_response_mailer.rb
+++ app/mailers/automated_response_mailer.rb
@@ -1,11 +1,11 @@
 class AutomatedResponseMailer < ActionMailer::Base
 
-  default from: "Компания ПетроСтройМонтаж <info@psm-spb.com>"
+  default from: "Компания ПетроСпецМонтаж <info@psm-spb.com>"
   
 
   def new_message(message)
     @message = message
-    mail subject: "ПетроСтройМонтаж",  to: @message.email
+    mail subject: "ПетроСпецМонтаж",  to: @message.email
   end
 
 end
